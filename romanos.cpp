ba6a913ce5421b1e3bb414df1558da47b8aa0aeb
Your Name Comes Here <you@yourdomain.example.com>
2024-04-25 22:49:26 -0300

    teste 3 (maioria das exceções já cobertas)

--- romanos.cpp
+++ romanos.cpp
@@ -4,18 +4,35 @@
 bool checar(std::string n) {
     char apoio[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
     bool juri = true;
+    char anterior1 = 0;
+    char atual1 = 0;
+    int cont = 0;
     for (char i : n) {
         int contador = 0;
         for (char u : apoio) {
             if (u == i) {
+                atual1 = u;
                 break;
             }
             contador += 1;
+        }
+        if (atual1 == anterior1) {
+            cont +=1;
+        } else {
+            cont=1;
         }
         if (contador == 7) {
             juri = false;
             break;
         }
+        if (cont > 3 && (anterior1=='I'||anterior1=='X'||anterior1=='C'||anterior1=='M')) {
+            juri=false;
+            break;
+        } else if (cont > 1 && (anterior1=='V'||anterior1=='L'||anterior1=='D')) {
+            juri=false;
+            break;
+        }
+        anterior1=atual1;
     }
     return juri;
 }
@@ -24,19 +41,38 @@ int romanos_para_decimal(std::string num_romano) {
     long apoio1[] = {1, 5, 10, 50, 100, 500, 1000};
     long resp = 0;
     long tam = num_romano.length();
+    int atual = 0;
+    int anterior = 0;
+    bool juri = true;
     if (checar(num_romano) == true) {
-        for (int u = 0; u < tam; u += 1) {
+        for (int u = (tam-1); u >= 0; u -= 1) {
             for (int i = 0; i < 7; i += 1) {
                 if (apoio[i] == num_romano[u]) {
-                    resp += apoio1[i];
+                    atual = apoio1[i];
                     break;
                 }
             }
+            if (atual < anterior) {
+                 if (anterior - atual == atual) {
+                    juri=false;
+                    break;
+                } else {
+                    resp -= atual;
+                }
+            } else {
+                resp+=atual;
+            }
+            anterior = atual;
         }
     } else {
         resp = -1;
     }
-    return resp;
+    if (juri==true) {
+        return resp;
+    } else {
+        return -1;
+    }
+
 }
 
 
--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -33,11 +33,52 @@ TEST_CASE( "Numeros romanos - algarismos multiplos", "[romanos]") {
 
     REQUIRE( romanos_para_decimal("MDCLXIII") == 1663);
 }
+
+TEST_CASE( "Numeros romanos - algarismos multiplos com subtração válida", "[romanos]") {
+
+    REQUIRE( romanos_para_decimal("IV") == 4);
+
+    REQUIRE( romanos_para_decimal("IX") == 9);
+
+    REQUIRE( romanos_para_decimal("CMXCIX") == 999);
+
+    REQUIRE( romanos_para_decimal("CMXC") == 990);
+
+    REQUIRE( romanos_para_decimal("CMXC") == 990);
+
+    REQUIRE( romanos_para_decimal("XL") == 40);
+}
+
 TEST_CASE( "Numeros romanos - algarismos inválidos", "[romanos]" ) {
     REQUIRE( romanos_para_decimal("G") == -1 );
 
     REQUIRE( romanos_para_decimal("i") == -1 );
 
+    REQUIRE( romanos_para_decimal("x") == -1);
 
+    REQUIRE( romanos_para_decimal("A") == -1);
+
+    REQUIRE( romanos_para_decimal("O") == -1);
 }
+TEST_CASE( "Numeros romanos - repetições inválidas", "[romanos]") {
+
+    REQUIRE( romanos_para_decimal("VV") == -1);
+
+    REQUIRE( romanos_para_decimal("IIII") == -1);
+
+    REQUIRE( romanos_para_decimal("VIIII") == -1);
+
+    REQUIRE( romanos_para_decimal("IIIIV") == -1);
+
+    REQUIRE( romanos_para_decimal("XVIIII") == -1);
+}
+
+TEST_CASE( "Numeros romanos - subtração inválida", "[romanos]") {
+    REQUIRE( romanos_para_decimal("VX") == -1);
+
+    REQUIRE( romanos_para_decimal("DM") == -1);
+
+    REQUIRE( romanos_para_decimal("LC") == -1);
+}
+
 
